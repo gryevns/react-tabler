b26d2db416f4b8f49a949ec21df95854bc498d20
Gary Evans <garyevans1990@gmail.com>
2020-05-12 20:13:44 +0100

    Add basic table snapshot tests

--- src/Table/index.test.tsx
+++ src/Table/index.test.tsx
@@ -1,11 +1,34 @@
 import React from "react";
-import { shallow, mount } from "enzyme";
+import { shallow } from "enzyme";
 
 import Table from "./index";
 
 describe("Table", () => {
     it("should match snapshot", () => {
-        // TODO
-        return;
+        const wrapper = shallow(
+            <Table>
+                <Table.Header>
+                    <Table.ColHeader>User</Table.ColHeader>
+                </Table.Header>
+                <Table.Body>
+                    <Table.Row>
+                        <Table.Col>Ronald Bradley</Table.Col>
+                    </Table.Row>
+                </Table.Body>
+            </Table>,
+        );
+        expect(wrapper).toMatchSnapshot();
+    });
+    it("should match striped snapshot", () => {
+        const wrapper = shallow(<Table striped>test</Table>);
+        expect(wrapper).toMatchSnapshot();
+    });
+    it("should match cards snapshot", () => {
+        const wrapper = shallow(<Table cards>test</Table>);
+        expect(wrapper).toMatchSnapshot();
+    });
+    it("should match hight snapshot", () => {
+        const wrapper = shallow(<Table highlightRowOnHover>test</Table>);
+        expect(wrapper).toMatchSnapshot();
     });
 });
